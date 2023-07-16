bc3fd40e861ba929bd41f3919a11a6f924b4ce01
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2023-07-16 08:51:13 -0600

    Initial commit

---
+++ tests/erc-6551-registry.test.ts
@@ -0,0 +1,94 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { AccountCreated } from "../generated/ERC6551Registry/ERC6551Registry"
+import { handleAccountCreated } from "../src/erc-6551-registry"
+import { createAccountCreatedEvent } from "./erc-6551-registry-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let account = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let implementation = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let chainId = BigInt.fromI32(234)
+    let tokenContract = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let tokenId = BigInt.fromI32(234)
+    let salt = BigInt.fromI32(234)
+    let newAccountCreatedEvent = createAccountCreatedEvent(
+      account,
+      implementation,
+      chainId,
+      tokenContract,
+      tokenId,
+      salt
+    )
+    handleAccountCreated(newAccountCreatedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "account",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "implementation",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "chainId",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "tokenContract",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "tokenId",
+      "234"
+    )
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "salt",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
