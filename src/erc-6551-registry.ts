7d41b03509fc928647d0aa91d8158d8102560007
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2023-07-16 09:39:24 -0600

    Add factory contract

--- src/erc-6551-registry.ts
+++ src/erc-6551-registry.ts
@@ -1,50 +1,19 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   ERC6551Registry,
-  AccountCreated
-} from "../generated/ERC6551Registry/ERC6551Registry"
-import { ExampleEntity } from "../generated/schema"
+  AccountCreated,
+} from "../generated/ERC6551Registry/ERC6551Registry";
+import { Account } from "../generated/schema";
 
 export function handleAccountCreated(event: AccountCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Account.load(event.params.account.toHex());
+  if (entity) return;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-  entity.implementation = event.params.implementation
+  entity = new Account(event.params.account.toHex());
+  entity.chainID = event.params.chainId;
+  entity.tokenContract = event.params.tokenContract;
+  entity.tokenID = event.params.tokenId;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.account(...)
-  // - contract.createAccount(...)
-  // - contract.userNullifiers(...)
+  entity.save();
 }
